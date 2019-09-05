181f8d8f9bd0cbcfdda65e5e774fb3e3a28cdc9e
Nadav Strahilevitz <nd.strahilevitz@gmail.com>
2019-09-06 01:19:21 +0300

    added pop and adherence to new instuction design

--- main.c
+++ main.c
@@ -1,35 +1,31 @@
 #include "stdio.h"
 #include "stdlib.h"
 #include "stack.h"
-#define STACK_SIZE 512
+#define STACK_SIZE 0xfff
 #define PROGRAM_SIZE 512
 
 uint32_t program[PROGRAM_SIZE] = 
-{0x200005dc,
-0x4f010000,
-0x4402000f,
+{0x0a0005dc,
+0x020f002d,
+0x021d0000,
+0x0224000f,
 0x0};
 
 //define opcodes
 typedef enum{
-    END, //no arg
-    NOP, //no arg
-    PUSH, //one arg, value to push
-    POP, //one arg, register to load to
-    MOV, //two args, dest reg and src value or reg
-    ADD, //two args
-    SUB, //two args
-    MUL, //two args
-    DIV, //two args
-    JMP, //one arg
-    JNZ, //one arg
-    TEST, //one arg
-    NOT, //one arg
-    AND, //two args
-    OR, //two args
-    XOR //two args
+    END = 0, NOP, MOV, 
+    PUSH = 10, POP, 
+    ADD = 20, SUB, MUL, DIV, 
+    TEST = 30, JMP, JNZ, JZ,
+    NOT = 40, AND, OR, XOR,
+    PRINT = 50
 } OPCODE;
 
+/*
+    R0 - ACCUMULATOR
+    R1 - ACCUMULTOR
+    R12 - FLAGS
+ */
 typedef enum{
     R0,
     R1,
@@ -51,7 +47,7 @@ typedef enum{
 } REGISTER;
 
 //define and init the general registers
-uint16_t regs[REGS_NUMBER];
+uint32_t regs[REGS_NUMBER];
 
 //stack base pointer
 int* bp;
@@ -60,7 +56,7 @@ int* bp;
 int* ip;
 
 //stack pointer
-uint16_t* sp;
+uint32_t* sp;
 
 //define stack
 Stack* st;
@@ -75,14 +71,13 @@ void init_vm(){
 }
 
 void free_vm(){
-    free(st);
+    stack_free(st);
     //free(program);
 }
 
-//TODO: create scopes and rewrite the opcodes to be readable
 void cpu_cycle(){
     uint32_t instr = *ip++;
-    uint32_t opcode = (instr >> 28 &0xf);
+    uint32_t opcode = (instr >> 24 &0xff);
     switch(opcode){
         case END:
             run = 0;
@@ -98,11 +93,12 @@ void cpu_cycle(){
         case POP:{
             int dest = instr & 0xf;
             regs[dest] = stack_pop(st);
+            sp = &st->arr[st->top];
             break;
         }
         case MOV:{
-            int flag = instr >> 16 &0x3;
-            int dest = instr >> 24 & 0xf;
+            int flag = instr >> 20 &0x3;
+            int dest = instr >> 16 & 0xf;
             if(flag == 0){                
                 uint16_t val = instr & 0xffff;
                 regs[dest] = val;
@@ -128,6 +124,7 @@ int main(){
     while(run){
         cpu_cycle();
     }
-    printf("%d", regs[R5]);
+    printf("%d", regs[R13]);
+    free_vm();
     return 0;
 }

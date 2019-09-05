2d46f24206c2f2363d5cb99727dd1552d1730b12
Nadav Strahilevitz <nd.strahilevitz@gmail.com>
2019-09-06 01:18:30 +0300

    changed to 32bit values, fixed a MAJOR malloc bug

--- stack.c
+++ stack.c
@@ -1,14 +1,23 @@
 #include "stack.h"
-#include  <stdlib.h>
+#include <stdlib.h>
 
 Stack* stack_new(int cap){
-    Stack* s = (Stack*)malloc(sizeof(sizeof(Stack)));
+    Stack* s = (Stack*)malloc(sizeof(Stack));
+
+    if(!s){
+        exit(FAILED_TO_ALLOCATE_STACK);
+    }
 
     s->size = cap;
     s->top = -1;
-    s->arr = (uint16_t*)malloc(cap*sizeof(uint16_t));
+    s->arr = (uint32_t*)malloc(cap*sizeof(uint32_t));
 
     return s;
+}
+
+void stack_free(Stack* s){
+    free(s->arr);
+    free(s);
 }
 
 int stack_is_empty(Stack* s){
@@ -19,21 +28,21 @@ int stack_is_full(Stack* s){
     return s->top == s->size - 1;
 }
 
-uint16_t stack_peek(Stack* s){
+uint32_t stack_peek(Stack* s){
     if(stack_is_empty(s)){
         exit(MEMORY_OUT_OF_BOUND);
     }
     return s->arr[s->top];
 }
 
-void stack_push(Stack* s, uint16_t val){
+void stack_push(Stack* s, uint32_t val){
     if(stack_is_full(s)){
         exit(OVERFLOW_ERROR);
     }
     s->arr[++s->top] = val;
 }
 
-uint16_t stack_pop(Stack* s){
+uint32_t stack_pop(Stack* s){
     if(stack_is_empty(s)){
         exit(OVERFLOW_ERROR);
     }
--- stack.h
+++ stack.h
@@ -1,22 +1,23 @@
-#include "exitcodes.h"
-#include <stdint.h>
-
 #ifndef STACK_H_
 #define STACK_H_
 
-//a fixed sized stack of 16 bit numbers
+#include "exitcodes.h"
+#include <stdint.h>
+
+//a fixed sized stack of 32 bit numbers
 typedef struct stack{
     int size; //size of stack
     int top; //index of top value
-    uint16_t* arr; //array where stack is stored
+    uint32_t* arr; //array where stack is stored
 } Stack;
 
 Stack* stack_new(int cap);
+void stack_free(Stack *s);
 int stack_is_empty(Stack* s);
 int stack_is_full(Stack* s);
-uint16_t stack_peek(Stack* s);
-void stack_push(Stack* s, uint16_t val);
-uint16_t stack_pop(Stack* s);
+uint32_t stack_peek(Stack* s);
+void stack_push(Stack* s, uint32_t val);
+uint32_t stack_pop(Stack* s);
 
 #endif
 

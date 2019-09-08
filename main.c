527e4833009955b2f736ee8d073b333b799a9b06
Nadav Strahilevitz <nd.strahilevitz@gmail.com>
2019-09-09 01:00:35 +0300

    tested the flags

--- main.c
+++ main.c
@@ -1,7 +1,12 @@
+#include "vm.h"
 #include "stdio.h"
-#include "stdlib.h"
 
 
 int main(){
-    return 0;
+    vm_t* vm = vm_create();
+    vm_set_flag(vm, CF);
+    vm_set_flag(vm, ZF);
+    vm_set_flag(vm, OF);
+    vm_set_flag(vm, NF);
+    print_vm_details(vm);
 }
